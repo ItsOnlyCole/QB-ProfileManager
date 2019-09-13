f10ad185589cd000a3dd30b9650555522cb08917
Cole Hemp <ColeHemp@gmail.com>
2019-09-12 23:24:37 -0400

    WIP: Add way to populate list with profiles.xml

--- profileManager.py
+++ profileManager.py
@@ -2,17 +2,35 @@
 import sys
 from PyQt5 import QtWidgets, uic
 
+profilesFile = "/home/itsonlycole/.config/QB-ProfileManager/profiles.xml"
+
 class Ui(QtWidgets.QMainWindow):
     def __init__(self):
         super(Ui, self).__init__() #Calls the inherited classes __init__ method
         uic.loadUi('mainUI.ui', self) #Loads the .ui file
 
         self.profilesList = self.findChild(QtWidgets.QListWidget, 'profilesListWidget')
-        self.profilesList.addItem('Test')
-        self.profilesList.addItem('CoolerTest')
         
         self.show() #Shows the gui
+
+    #def loadProfilesList(profiles):
+        #self.profilesList.clear()
+        #for profile in profiles:
+            #self.profilesList.addItem(profile)
+
+def createProfilesArray(file):
+    profilesXML = open(file, "r")
+    profilesText = profilesXML.readlines()
+    profilesList = []
+    #profilesLength = len(profilesText)
+    for profile in profilesText:
+        profilesList.append(profile)
+    return profilesList
+
 app = QtWidgets.QApplication(sys.argv) #Creates an instance of QtWidgets.QApplication
 window = Ui() #Creates an instance of our class
-window.profilesList.addItem('Pickachu')
+profiles = createProfilesArray(profilesFile)
+window.profilesList.clear()
+for profile in profiles:
+    window.profilesList.addItem(profile)
 app.exec_()
