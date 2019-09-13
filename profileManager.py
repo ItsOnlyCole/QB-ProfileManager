1d8849cbf44c7d828dc4fdf496f42e5e2794230c
Cole Hemp <ColeHemp@gmail.com>
2019-09-13 13:32:20 -0400

    Finish method to load profiles into QListWidget

--- profileManager.py
+++ profileManager.py
@@ -8,28 +8,24 @@ class Ui(QtWidgets.QMainWindow):
     def __init__(self):
         super(Ui, self).__init__() #Calls the inherited classes __init__ method
         uic.loadUi('mainUI.ui', self) #Loads the .ui file
-
         self.profilesList = self.findChild(QtWidgets.QListWidget, 'profilesListWidget')
-        
         self.show() #Shows the gui
 
-    #def loadProfilesList(profiles):
-        #self.profilesList.clear()
-        #for profile in profiles:
-            #self.profilesList.addItem(profile)
-
-def createProfilesArray(file):
+def createProfilesList(file):
     profilesXML = open(file, "r")
     profilesText = profilesXML.readlines()
     profilesList = []
-    #profilesLength = len(profilesText)
+    profilesTextLength = len(profilesText)
+    profilesTextLength -= 1
+    profilesText.pop(0)
+    profilesText.pop(profilesTextLength-1)
     for profile in profilesText:
-        profilesList.append(profile)
+        profilesList.append(profile[11:-11])
     return profilesList
 
 app = QtWidgets.QApplication(sys.argv) #Creates an instance of QtWidgets.QApplication
 window = Ui() #Creates an instance of our class
-profiles = createProfilesArray(profilesFile)
+profiles = createProfilesList(profilesFile)
 window.profilesList.clear()
 for profile in profiles:
     window.profilesList.addItem(profile)
